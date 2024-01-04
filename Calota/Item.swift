d220106511201721d74d5294eefcdeac10f2de82
deorhan <deorhan@edu.aau.at>
2024-01-04 15:17:24 +0100

    filter applied to stored image
    filter applied to image you want to store

--- Calota/Item.swift
+++ Calota/Item.swift
@@ -1,6 +1,8 @@
 import Foundation
 import SwiftData
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 @Model
 final class Item: Identifiable{
@@ -18,7 +20,7 @@ final class Item: Identifiable{
     var salz: Double
     var image: Data?
     
-    init(date: String, calories: Double, divFett: Double, ungFett: Double, gesFett: Double, kohlen: Double, zcker: Double, eiweis: Double, salz: Double, image: UIImage? = nil) {
+    init(date: String, calories: Double, divFett: Double, ungFett: Double, gesFett: Double, kohlen: Double, zcker: Double, eiweis: Double, salz: Double, image: UIImage?) {
         self.date = date
         self.calories = calories
         self.divFett = divFett
@@ -28,7 +30,7 @@ final class Item: Identifiable{
         self.zcker = zcker
         self.eiweis = eiweis
         self.salz = salz
-        if let image = image {self.image = image.pngData()}
+        self.image = applyFilter(to: image ?? UIImage()).pngData()
     }
     
     func getImage() -> UIImage? {
@@ -36,5 +38,28 @@ final class Item: Identifiable{
             return UIImage(data: imageData)
         }
         return nil
+    }
+    
+    //CoreImage
+    func applyFilter(to inputImage: UIImage) -> UIImage {
+        //convert input image (uiimage) to ciimage (because CoreImage uses ciimage)
+        guard let ciImage = CIImage(image: inputImage) else { return inputImage }
+        
+        //apply color adjustment filter
+        let colorAdjustmentFilter = CIFilter.colorControls()
+        colorAdjustmentFilter.inputImage = ciImage
+        colorAdjustmentFilter.contrast = 1.5
+        colorAdjustmentFilter.saturation = 1.2
+        colorAdjustmentFilter.brightness = 0.2
+
+        guard let outputCIImage = colorAdjustmentFilter.outputImage else { return inputImage }
+        
+        //convert filtered ciimage to cgimage
+        let context = CIContext()
+        guard let cgImage = context.createCGImage(outputCIImage, from: outputCIImage.extent) else {
+            return inputImage
+        }
+        //create new uiimage from cgimage
+        return UIImage(cgImage: cgImage)
     }
 }
